3fe86385b0afe2cbd42b803952c5f9a0803c0d84
Nghiêm Huy Hưng 11222608 <66253501+Nghiem-HuyHung-11222608@users.noreply.github.com>
2025-07-10 17:52:11 +0700

    Delete src directory

--- src/main/java/com/lms/exam/repository/ExamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lms.exam.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.lms.exam.model.Exam;
-
-public interface ExamRepository extends JpaRepository<Exam, Long> {
-}
--- src/main/java/com/lms/exam/repository/QuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lms.exam.repository;
-
-import com.lms.exam.model.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-}
